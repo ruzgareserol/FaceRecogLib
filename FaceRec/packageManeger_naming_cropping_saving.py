1cf517dd19f82b216dfc4746758c158fb1b5ef03
ruzgareserol <43060781+ruzgareserol@users.noreply.github.com>
2018-09-11 11:26:42 +0000

    Delete packageManeger_naming_cropping_saving.py

--- FaceRec/packageManeger_naming_cropping_saving.py
+++
@@ -1,9 +0,0 @@
-import cropNsave
-import renameFilesAsIndexes
-imFolder = r'/home/nvidia/Desktop/ImageData/latif'
-cropNsave.cropAndSaveThemIntoSpecificFaceFolders(imFolder,0,"latif")
-imFolder = r'/home/nvidia/Desktop/ImageData/orhun'
-cropNsave.cropAndSaveThemIntoSpecificFaceFolders(imFolder,0,"orhun")
-imFolder = r'/home/nvidia/Desktop/ImageData/ruzgar'
-cropNsave.cropAndSaveThemIntoSpecificFaceFolders(imFolder,0,"ruzgar")
-
